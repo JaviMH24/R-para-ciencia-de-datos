3cf762e70a0d764db18c1670afa9df9b9e0006e8
ERASG Academy <alphajhm301@gmail.com>
2026-07-13 14:35:58 -0600

    Add files via upload

---
+++ Capitulo_4_Concimientos_basicos.R
@@ -0,0 +1,53 @@
+# **Capitulo 4 concoimientos basicos de R**
+
+# **Sección 4.1**
+
+#R se puede utilizar como una calculadora
+
+1/200*30
+
+(59 + 73 + 2) / 3
+
+sin(pi / 2)
+
+#Para asignar valores a determindas variables
+#se genera por medio de una sintaxis de flecha
+
+x <- 3*4
+
+#las instrucciones de asignación, tienen la
+#misma estructura:
+
+nombre_objeto <- valor
+
+#La asignación de valores se puede
+#recordar de esta manera:
+#Cuando leas esa línea de código di mentalmente 
+#“nombre_objeto recibe valor”.
+
+#ATAJO DE TECLADO "Alt" + "-" para generar rapidamente
+#la flecha
+
+#-------------------------------------------------------
+
+#Sección 4.2 **La importancia de los nombres**
+
+#Los nombres de los objetos deben comenzar con una letra y 
+#solo pueden contener letras, números, "_" y ".". Por ejemplo:
+
+
+yo_uso_guion_bajo
+OtraGenteUsaMayusculas
+algunas.personas.usan.puntos
+Y_algunasPocas.Personas_RENIEGANdelasconvenciones
+
+
+#Podemos examinar un objeto, volviendo a llamar solo escribiendo
+#nombre
+
+x
+
+este_es_un_nombre_largo <- 2.5
+
+este_es_un_nombre_largo
+
